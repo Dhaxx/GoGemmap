eddc4643b851f621d6e586c4e65042807d9ac014
Dhaxx <kaiopablo44@gmail.com>
2025-05-26 15:42:24 -0300

    Adiciona arquivos de configuração e implementa conexões com Firebird e Oracle

---
+++ connection/conexao.go
@@ -0,0 +1,62 @@
+package connection
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "path/filepath"
+
+    "github.com/joho/godotenv"
+    "github.com/jmoiron/sqlx"
+    _ "github.com/godror/godror"          // Driver Oracle
+    _ "github.com/nakagami/firebirdsql"   // Driver Firebird
+    "database/sql"
+)
+
+var dsnFdb string
+var dsnOracle string
+
+func init() {
+    envPath, err := os.Getwd()
+    if err != nil {
+        log.Fatalf("Erro ao obter diretório: %v", err)
+    }
+
+    if err = godotenv.Load(filepath.Join(envPath, ".env")); err != nil {
+        log.Fatalf("Erro ao carregar .env: %v", err)
+    }
+
+    // Firebird
+    dsnFdb = fmt.Sprintf("%s:%s@%s:%s/%s?charset=win1252&auth_plugin_name=Legacy_Auth",
+        os.Getenv("FDB_USER"),
+        os.Getenv("FDB_PASS"),
+        os.Getenv("FDB_HOST"),
+        os.Getenv("FDB_PORT"),
+        os.Getenv("FDB_PATH"))
+
+    // Oracle (usando SID)
+    dsnOracle = fmt.Sprintf(`user="%s" password="%s" connectString="%s:%s/%s"`,
+        os.Getenv("ORA_USER"),
+        os.Getenv("ORA_PASS"),
+        os.Getenv("ORA_HOST"),
+        os.Getenv("ORA_PORT"),
+        os.Getenv("ORA_SERVICE"))
+}
+
+// GetConexoes retorna conexão Firebird via sql e Oracle via sqlx
+func GetConexoes() (*sql.DB, *sqlx.DB, error) {
+    // Conexão com Firebird
+    ConexaoFdb, err := sql.Open("firebirdsql", dsnFdb)
+    if err != nil {
+        return nil, nil, fmt.Errorf("erro ao estabelecer conexão FDB: %v", err)
+    }
+
+    // Conexão com Oracle
+    ConexaoOracle, err := sqlx.Connect("godror", dsnOracle)
+    if err != nil {
+        ConexaoFdb.Close()
+        return nil, nil, fmt.Errorf("erro ao estabelecer conexão Oracle: %v", err)
+    }
+
+    return ConexaoFdb, ConexaoOracle, nil
+}
---
+++ go.mod
@@ -0,0 +1,27 @@
+module GoGemmap
+
+go 1.23.0
+
+toolchain go1.23.9
+
+require (
+	github.com/godror/godror v0.48.3
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/nakagami/firebirdsql v0.9.15
+)
+
+require (
+	github.com/VictoriaMetrics/easyproto v0.1.4 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/godror/knownpb v0.3.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/nakagami/chacha20 v0.1.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	gitlab.com/nyarla/go-crypt v0.0.0-20160106005555-d9a5dc2b789b // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+	modernc.org/mathutil v1.4.2-0.20220822142738-b13e5b564332 // indirect
+)
